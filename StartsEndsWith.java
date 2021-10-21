3445b013c095f296fa12170b68e507d37cfc337e
Pbaid <89845003+Pbaid@users.noreply.github.com>
2021-10-21 17:30:54 +0530

    Add files via upload

---
+++ StartsEndsWith.java
@@ -0,0 +1,11 @@
+import java.utils.*;
+
+public class StartsEndsWith {
+	
+	public static void main(String[] args) {
+		
+		String str = "Paras Baid";
+		boolean res = (str.startsWith("P")) & (str.endsWith("d"));
+		System.out.println(res);
+	}
+}
